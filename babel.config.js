6771ad1c7d3bffe9c2e3cfaa5289fa1bfbf0b155
tandemkids <77397113+tandemkids@users.noreply.github.com>
2026-03-24 14:51:02 -0700

    feat: add react-native-worklets-core babel plugin for frame processors

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    presets: ['module:@react-native/babel-preset'],
-  };
-  
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    // Required for react-native-vision-camera frame processors
+    // Must come before any other plugins that transform arrow functions
+    'react-native-worklets-core/plugin',
+  ],
+};
