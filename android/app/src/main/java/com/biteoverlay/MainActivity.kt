dabb600ae05ca087137f582b2f0f961bdccee8fe
tandemkids <77397113+tandemkids@users.noreply.github.com>
2026-03-24 00:12:00 -0700

    chore: add android/app/src/main/java/com/biteoverlay/MainActivity.kt

---
+++ android/app/src/main/java/com/biteoverlay/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.biteoverlay
+
+  import com.facebook.react.ReactActivity
+  import com.facebook.react.ReactActivityDelegate
+  import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+  import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+  class MainActivity : ReactActivity() {
+
+    override fun getMainComponentName(): String = "BiteOverlay"
+
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+  }
+  
